ff158adf51587a49eba58177373b2283c398d08a
prashantlotlikar <prashant.lotlikar@gmail.com>
2023-07-29 16:43:40 +0530

    Added 3 new models

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("added ML Code here")
+
+def model1():
+    print("Adding code for model 1")
+
+def model2():
+    print("Adding code for model 2")
+
+def model3():
+    print("Adding code for model 3")
